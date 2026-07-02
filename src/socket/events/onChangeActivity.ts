85924911f6d221e5cb2cde75264800b56bc81ff9
Supertiger <saawalkhn@googlemail.com>
2026-07-02 09:33:29 +0100

    fix

--- src/socket/events/onChangeActivity.ts
+++ src/socket/events/onChangeActivity.ts
@@ -20,17 +20,21 @@ const Activity = type({
 });
 type Activity = typeof Activity.infer;
 
-export const debouncedUpdateAndEmitActivity = debounceByKey(updateAndEmitActivity, 1000, (userId) => userId, 1000);
+export const debouncedChangeActivity = debounceByKey(changeActivity, 1000, (userId) => userId, 1000);
 
 export async function onChangeActivity(socket: Socket, payload: Activity[] | null) {
   const userId = await getUserIdBySocketId(socket.id);
   if (!userId) return;
 
+  debouncedChangeActivity(userId, socket, payload);
+}
+
+function changeActivity(userId: string, socket: Socket, payload: Activity[] | null) {
   if (payload && !Array.isArray(payload)) {
     payload = [payload];
   }
   if (!payload) {
-    debouncedUpdateAndEmitActivity(userId, socket.id, null);
+    updateAndEmitActivity(userId, socket.id, null);
     return;
   }
   const activities: ActivityStatus[] = [];
@@ -80,7 +84,7 @@ export async function onChangeActivity(socket: Socket, payload: Activity[] | nul
     activities.push({ ...out, socketId: socket.id });
   });
 
-  debouncedUpdateAndEmitActivity(userId, socket.id, activities);
+  updateAndEmitActivity(userId, socket.id, activities);
 }
 
 async function updateAndEmitActivity(userId: string, socketId: string, activities: ActivityStatus[] | null) {
